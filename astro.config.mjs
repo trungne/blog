4a6e48a2840dbd7779ca43ef9f68940acfd22c74
trungne <nguyenquochoangtrung@gmail.com>
2022-10-14 13:29:39 +0700

    Revert "chore: add adapter"
    This reverts commit c5ce135c059de3162e18254d4a3c55d6d909b8f9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel()
+  output: 'server'
 });
