989ec8d4da18fe45c7aa7ec550601d11c36f9d3a
trungne <nguyenquochoangtrung@gmail.com>
2022-10-14 17:30:37 +0700

    chore: add text color for dark mode

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,18 @@
 import type React from "react"
 import cx from "classnames"
-import DarkModeSwap from "./DarkModeSwap"
-
-
 
 type MenuProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 const Menu: React.FC<MenuProps> = ({ className, ...rest }) => {
     return (
         <div {...rest} className={cx("flex-none", className)}>
             <ul className="menu menu-horizontal p-0">
-                <MenuItems className="hidden md:block" />
+                <MenuItems className="hidden md:block dark:text-primary-content" />
                 <li className="md:hidden relative">
                     <button className="btn btn-square btn-ghost p-0">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </button>
-                    <ul className="absolute top-12 right-0 p-2 bg-green-sheen z-10">
-                        <MenuItems />
+                    <ul className="absolute top-12 right-0 p-2 bg-independence dark:bg-eggshell z-10">
+                        <MenuItems className="dark:text-primary-content" />
                     </ul>
                 </li>
             </ul>
