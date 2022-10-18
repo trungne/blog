2234e4b0f8badc53e69d405ec0aea94dfea6cf1b
trungne <nguyenquochoangtrung@gmail.com>
2022-10-18 16:20:22 +0700

    fix: styles

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -4,7 +4,7 @@ import profileMobile from './profile-mobile.jpg'
 export default () => {
     return (
         <div className="avatar block md:my-8">
-            <div className="w-44 md:w-64 rounded-full mx-auto">
+            <div className="w-44 h-44 md:h-64 md:w-64 rounded-full mx-auto">
                 <picture>
                     <source srcSet={profilePC} media="(min-width: 768px)" />
                     <img src={profileMobile} alt="my awesome avatar" />
