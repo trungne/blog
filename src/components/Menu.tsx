5680d7f690ed317b0dff200ec200c5c6b24bcec2
trungne <nguyenquochoangtrung@gmail.com>
2023-01-28 23:45:00 +0700

    chore: add highlight

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,16 +4,21 @@ import cx from "classnames";
 type MenuProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
->;
-const Menu: React.FC<MenuProps> = ({ className, ...rest }) => {
+> & {
+  path: string;
+};
+const Menu: React.FC<MenuProps> = ({ className, path, ...rest }) => {
   return (
     <>
       <div {...rest} className={cx("flex-none", className)}>
         <ul className="menu menu-horizontal p-0">
-          <MenuItems className="hidden md:block default-text font-thin" />
+          <MenuItems
+            path={path}
+            className="hidden md:block default-text font-thin"
+          />
         </ul>
       </div>
-      
+
       <div className="md:hidden dropdown dropdown-end">
         <label
           tabIndex={0}
@@ -37,28 +42,44 @@ const Menu: React.FC<MenuProps> = ({ className, ...rest }) => {
           tabIndex={0}
           className="dropdown-content menu rounded !w-[180px] bg-base-content dark:bg-neutral-content"
         >
-          <MenuItems className="default-text" />
+          <MenuItems path={path} className="default-text" />
         </ul>
       </div>
     </>
   );
 };
 
-const MenuItems: React.FC<
-  React.DetailedHTMLProps<React.LiHTMLAttributes<HTMLLIElement>, HTMLLIElement>
-> = (props) => {
+type MenuItemsProps = React.DetailedHTMLProps<
+  React.LiHTMLAttributes<HTMLLIElement>,
+  HTMLLIElement
+> & {
+  path: string;
+};
+const MenuItems: React.FC<MenuItemsProps> = ({ path, ...props }) => {
   return (
     <>
-      <li {...props}>
-        <a href="/about">About Me</a>
-      </li>
-      <li {...props}>
-        <a href="/blog">Stuff I wrote</a>
-      </li>
-      <li {...props}>
-        <a href="/portfolio">Portfolio</a>
-      </li>
+      <MenuItem path={path} href="/about" label="About Me" {...props} />
+      <MenuItem path={path} href="/blog" label="Stuff I wrote" {...props} />
+      <MenuItem path={path} href="/portfolio" label="Portfolio" {...props} />
     </>
+  );
+};
+
+const MenuItem = ({
+  path,
+  href,
+  label,
+  ...props
+}: MenuItemsProps & { href: string; label: string }) => {
+  return (
+    <li
+      {...props}
+      className={cx(
+        path === href ? "text-accent" : "default-text"
+      )}
+    >
+      <a href={href}>{label}</a>
+    </li>
   );
 };
 
