1656aa2c72db62808440459b2dd06d58cd37aa56
trungne <nguyenquochoangtrung@gmail.com>
2023-01-28 22:04:22 +0700

    chore: use constant

---
+++ src/utils/constant.ts
@@ -0,0 +1,2 @@
+export const DARK_MODE_COOKIE = "darkMode";
+export const NAV_BAR_HEIGHT = 64;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,10 @@
-const ONE_YEAR_IN_SECONDS = 60 * 60 * 24 * 365 // roughly one year
+import { DARK_MODE_COOKIE } from "./constant"
 
+const ONE_YEAR_IN_SECONDS = 60 * 60 * 24 * 365 // roughly one year
 export const setDarkModeCookie = (isDarkMode: boolean) => {
     if (typeof document === 'undefined') {
         return
     }
 
-    document.cookie = `darkMode=${isDarkMode}; max-age=${ONE_YEAR_IN_SECONDS};`
+    document.cookie = `${DARK_MODE_COOKIE}=${isDarkMode}; max-age=${ONE_YEAR_IN_SECONDS};`
 }
