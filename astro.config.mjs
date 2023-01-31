d05a0b97676cadd8b334d9b987adadb34b2946fc
trungne <nguyenquochoangtrung@gmail.com>
2023-01-31 23:21:42 +0700

    chore: add cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+const env = loadEnv("", process.cwd(), ["STORYBLOK", "ASTRO"]);
+
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel()
+  server: {
+    port: +env.ASTRO_APP_PORT || 3000,
+  },
+  integrations: [
+    tailwind(),
+    react(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        ["HomePage"]: "components/Home/index",
+      },
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,15 +1,15 @@
-import profilePC from './profile-pc.jpg'
-import profileMobile from './profile-mobile.jpg'
+import profilePC from "./profile-pc.jpg";
+import profileMobile from "./profile-mobile.jpg";
 
 export default () => {
-    return (
-        <div className="avatar block md:my-8">
-            <div className="w-44 h-44 md:h-64 md:w-64 rounded-full mx-auto">
-                <picture>
-                    <source srcSet={profilePC} media="(min-width: 768px)" />
-                    <img src={profileMobile} alt="my awesome avatar" />
-                </picture>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="avatar block md:my-8">
+      <div className="mx-auto h-44 w-44 rounded-full md:h-64 md:w-64">
+        <picture>
+          <source srcSet={profilePC} media="(min-width: 768px)" />
+          <img src={profileMobile} alt="my awesome avatar" />
+        </picture>
+      </div>
+    </div>
+  );
+};
